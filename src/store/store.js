13fe1e35d8dcfeb3116bf1543b518d5177aa4afe
Neeraj Tomar <neeraj.tomar@knolskape.com>
2020-07-22 20:42:21 +0530

    added throttle to subscribe store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import Immutable from 'immutable';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import throttle from 'lodash/throttle';
 import rootReducer from '../reducers/rootReducer';
 import { loadState, saveState } from './sessionStorage';
 
@@ -8,8 +9,10 @@ const persistedState = Immutable.fromJS(loadState());
 
 const store = createStore(rootReducer, persistedState, applyMiddleware(thunk));
 
-store.subscribe(() => {
-    saveState(store.getState());
-});
+store.subscribe(
+    throttle(() => {
+        saveState({ newsFeed: store.getState().get('newsFeed') });
+    }, 1000)
+);
 
 export default store;
